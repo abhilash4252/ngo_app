c0362b364fa125bd87b6e1d66b7dd72c4593ec3a
abhilash4252 <abhilash4252@gmail.com>
2015-11-24 11:37:10 +0530

    Adding and deleteing organisations

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,18 +1,37 @@
 class OrganisationsController < ApplicationController
+  def index
+    @organisations = current_user.organisations.all
+  end
+
   def new
-    @organisation = Organisation.new
-    @organisation.user_id=current_user.id
+    @organisation = current_user.organisations.new
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+
+    if @organisation.user_id != current_user.id
+      redirect_to organisations_url
+    end
   end
 
   def create
-    @organisation = Organisation.new(organisation_params)
-    @organisation.user_id=current_user.id
-    @organisation.save
+    @organisation = current_user.organisations.new(organisation_params)
+    if @organisation.save
+      flash[:notice] = "Organisation created succesfully"
+      redirect_to @organisation
+    else
+      flash[:alert] = "Orgainisatio creation failed"
+      render :new
+    end
   end
-
+  def destroy
+    Organisation.find(params[:id]).destroy
+    flash[:success] = "Organisation deleted"
+    redirect_to organisations_url
+  end
   private
     def organisation_params
       params.require(:organisation).permit(:name,:user_id)
-      
     end
 end
